51f8e84ebbf763a2f4e057cafdc49ff7cdb7fd19
林少杰 <1335957573@qq.com>
2020-11-04 23:11:11 +0800

    引入van,protorem

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 import './assets/styles/icon.css'
 import './assets/styles/global.scss'
 import './assets/styles/mixin.scss'
+import './utils/vant'
 
 Vue.config.productionTip = false
 
